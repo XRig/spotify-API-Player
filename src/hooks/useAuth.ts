5f9d743ca991dbbc1d1eb21a5bc5c1910be6b6f2
Rex Righetti <rex@zepge.com>
2021-03-28 12:31:14 -0700

    Refactor useAuth hook for async await

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -6,16 +6,17 @@ interface Props {
 
 function UseAuth(props: Props) {
     const {code} = props
-    // const [accessToken,setAccessToken] = useState()
-    // const [refreshToken,setrefreshToken] = useState()
-    // const [expiresIn,setexpiresIn] = useState()
+    const [accessToken,setAccessToken] = useState()
+    const [refreshToken,setrefreshToken] = useState()
+    const [expiresIn,setexpiresIn] = useState()
 
 useEffect(() => {
-    console.log(code)
-     axios.post('http://localhost:3080/login',{
-        code
-    }).then(res => console.log(res.data)).catch(() =>{window.location.href = '/'})
-},[code])
-}
+    (async () => {
+        try {
+            const data = await axios.post('http://localhost:3080/login',{code})
+        } catch (error) { window.location.href = '/'}
+    })();
+    
+},[code])}
 
 export default UseAuth
