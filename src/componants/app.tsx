91bce8da5caf786add4bb3cde9b686d329057ed4
Rex Righetti <rex@zepge.com>
2021-03-28 11:27:08 -0700

    Add Spotify Login

---
+++ src/componants/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './login'
+
+interface Props {}
+
+function App(props: Props) {
+    const {} = props
+
+    return (
+        <Login/>
+    )
+}
+
+export default App
